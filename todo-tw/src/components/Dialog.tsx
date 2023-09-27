1f0057d0a56387cada40bdf63040254f62529152
Ebenezer <ebenezertesfaye@yahoo.com>
2023-09-27 16:27:20 +0300

    Changed the dailog to tailwind

--- todo-tw/src/components/Dialog.tsx
+++ todo-tw/src/components/Dialog.tsx
@@ -4,17 +4,18 @@ export type DialogProps = {
     children: React.ReactNode
 };
 const Dialog: React.FC<DialogProps> = (props: DialogProps) =>{
-    
+    //style={{zIndex: '10', width: '100vw', height: '100vh', background: 'rgba(0,0,0,0.3)'}}
     return <>
-            {props.show && <div style={{zIndex: '10', width: '100vw', height: '100vh', background: 'rgba(0,0,0,0.3)'}}>
-                <center>       
-                <div style={{borderRadius: '20px',  background: 'white', width: '50%',  height: '80%', position:'fixed', left: '25%', top: '10%'}}>
+            {props.show && <div className="z-10 w-screen h-screen bg-black/50 flex flex-col justify-center">
+                   
+                {/*style={{borderRadius: '20px',  background: 'white', width: '50%',  height: '80%', position:'fixed', left: '25%', top: '10%'}} */}
+                <div className="rounded-xl bg-white w-1/2 h-4/5 fixed left-1/4 top-1/10">
                     <div style={{display: 'flex', justifyContent: 'end', margin: '30px'}}>
                          <button className="icon-btn" onClick={props.onClose}>X</button>
                     </div>
                     {props.children}
                 </div>
-                </center>
+               
 
             </div>}
     </>
