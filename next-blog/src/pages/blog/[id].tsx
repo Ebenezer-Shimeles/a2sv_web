ff67f26ca729aec3b47ca0d6941e4c499d924100
Ebenezer <ebenezertesfaye@yahoo.com>
2023-09-28 14:48:07 +0300

    CRUD Done

--- next-blog/src/pages/blog/[id].tsx
+++ next-blog/src/pages/blog/[id].tsx
@@ -1,5 +1,6 @@
  
 import Head from 'next/head';
+import Link from 'next/link';
 import {useRouter} from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -24,13 +25,18 @@ import { useEffect, useState } from 'react';
             <title>Blog: {blogTitle}</title>
           </Head>
           <div className='h-screen w-screen flex flex-col justify-start mt-10 text-center gap-10'>
-            <h1 className='text-5xl font-bold underline'>{blogTitle}</h1>
+            <h1 className='text-5xl font-bold underline'>
+                {blogTitle} 
+               
+            </h1>
             <div className='self-center text-justify w-3/5 h-3/4 border-1 bg-slate-100 p-10 first-letter:capitalize first-letter:text-3xl 
                first-letter:font-bold
                rounded-2xl
             '>
                 {blogBody}
+                
             </div>
+            <Link href={`/edit/${id}`}><button className='text-2xl underline text-blue-500 '>Edit</button></Link>
           </div>
         </>
     )
